3d4cc68c47a7a84a4da6cbc07b9a14746a247640
alexaburchak <al5603bu-s@student.lu.se>
2025-02-03 16:54:58 +0100

    Update main translation script

--- RScripts/alligator_translate.R
+++ RScripts/alligator_translate.R
@@ -1,24 +1,53 @@
-library(Biostrings)
-library(dplyr)
-library(stringr)
+# Set CRAN mirror
+options(repos = c(CRAN = "https://cran.r-project.org"))
 
+# Check if package is installed (install it if not)
+install_if_needed <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg)
+  }
+}
+
+# Install packages 
+install_if_needed("dplyr")
+install_if_needed("stringr")
+
+if (!requireNamespace("Biostrings", quietly = TRUE)) {
+  if (!requireNamespace("BiocManager", quietly = TRUE)) {
+    install.packages("BiocManager")
+  }
+  BiocManager::install("Biostrings")
+}
+
+# Load packages
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(stringr)
+  library(Biostrings)
+})
+
+# Get command line arguments
 args <- commandArgs(trailingOnly = TRUE)
 fasta_file <- args[1]  
-count_output_dir <- args[2]
-fasta_output_dir <- args[3]  
+counts_fwd <- args[2]
+counts_rev <- args[3]
+fasta_output_dir <- args[4]  
+best_start_pos_file <- args[5]
 
 # Extract base name of input file
 base_name <- tools::file_path_sans_ext(basename(fasta_file))
 
-# Extract unique file naming (first occurrence of bc10XX)
-bc10XX <- str_extract(base_name, "bc10\\w+")
+# Extract start position from csv
+best_start_pos <- read.table(best_start_pos_file, header = FALSE, sep = ",")
+frame <- as.integer(best_start_pos[1, 1])
+cat("Translating from position:", frame)
 
 # Read the DNA sequences and compute reverse complement
 DNA_seqs <- readDNAStringSet(fasta_file)
 rev_DNA_seqs <- Biostrings::reverseComplement(DNA_seqs)
 
 # Function to save counts to csv 
-generate_counts <- function(sequence, output_dir, prefix){
+generate_counts <- function(sequence, output_filepath){
   # Convert DNAStringSet to df
   sequence_df <- data.frame(
     target_name = names(sequence),
@@ -27,43 +56,44 @@ generate_counts <- function(sequence, output_dir, prefix){
   
   # Generate count table 
   counts <- sequence_df %>%
-    group_by(target_name) %>%
+    group_by(Sequence) %>%
     summarise(
       count = n(),
-      Sequence = first(Sequence),
+      target_name = head(target_name, 1),
       .groups = 'drop'
     )
   
   # Save counts as CSV
-  output_file <- file.path(output_dir, paste0(prefix, "_counts.csv"))
-  write.csv(counts, file = output_file, row.names = FALSE)
+  write.csv(counts, file = output_filepath, row.names = FALSE)
 }
 
 # Function to translate sequences
-translate_frames <- function(fwd_seqs, rev_seqs, bc10XX, 
-                             output_dir, count_output_dir) {
-  for (frame in 1:3) {
-    # Subset and translate sequences
-    fsubseqs <- subseq(fwd_seqs, start = frame)
-    ftranslated <- Biostrings::translate(fsubseqs, if.fuzzy.codon = "X")
-    rsubseqs <- subseq(rev_seqs, start = frame)
-    rtranslated <- Biostrings::translate(rsubseqs, if.fuzzy.codon = "X")
-    
-    # Create output filenames
-    foutput_file <- file.path(output_dir, paste0(bc10XX, "_fwd_AA_frame", frame, ".fasta"))
-    routput_file <- file.path(output_dir, paste0(bc10XX, "_rev_AA_frame", frame, ".fasta"))
-    
-    # Write translated sequences to files
-    writeXStringSet(ftranslated, filepath = foutput_file, format = "fasta")
-    writeXStringSet(rtranslated, filepath = routput_file, format = "fasta")
-    message("Saved: ", foutput_file, " and ", routput_file)
+translate_frames <- function(fwd_seqs, rev_seqs, frame, base_name, 
+                             output_dir, counts_fwd, counts_rev) {
+  options(warn = -1)
 
-    # Generate counts for forward and reverse sequences
-    generate_counts(ftranslated, count_output_dir, paste0(bc10XX, "_fwd_frame", frame))
-    generate_counts(rtranslated, count_output_dir, paste0(bc10XX, "_rev_frame", frame))
-  }
+  # Subset and translate sequences
+  fsubseqs <- subseq(fwd_seqs, start = frame)
+  ftranslated <- Biostrings::translate(fsubseqs, if.fuzzy.codon = "X")
+  rsubseqs <- subseq(rev_seqs, start = frame)
+  rtranslated <- Biostrings::translate(rsubseqs, if.fuzzy.codon = "X")
+  
+  # Create output filenames
+  foutput_file <- file.path(output_dir, paste0(base_name, "_fwd_AA.fasta"))
+  routput_file <- file.path(output_dir, paste0(base_name, "_rev_AA.fasta"))
+  
+  # Write translated sequences to files
+  writeXStringSet(ftranslated, filepath = foutput_file, format = "fasta")
+  writeXStringSet(rtranslated, filepath = routput_file, format = "fasta")
+
+  # Generate counts for forward and reverse sequences
+  generate_counts(ftranslated, counts_fwd)
+  generate_counts(rtranslated, counts_rev)
+  
+  # Restore warning settings to default
+  options(warn = 0)
 }
 
 # Process forward and reverse strands
-translate_frames(DNA_seqs, rev_DNA_seqs, bc10XX, fasta_output_dir, count_output_dir)
-message("Processed and saved 6 FASTA files for: ", fasta_file)
+translate_frames(DNA_seqs, rev_DNA_seqs, frame, base_name, fasta_output_dir, counts_fwd, counts_rev)
+message("Processed and saved FASTA files for: ", fasta_file)
