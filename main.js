0fb8c70873e735a59ee737c90ea8e1158d6a5e41
alexaburchak <al5603bu-s@student.lu.se>
2025-02-25 10:04:09 +0100

    Add JSDoc comments

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ import path from "node:path";
 import csv from "csv-parser";
 import { get_config_by_path, get_config_path_by_args } from "./src/config.js";
 
-// NOTE: Clustering is currently commented out, hmmer runs on full fasta files
-
-// Define function to process ALL sequences from fastq
-// @ts-ignore
-async function fullSeqKit(fastq_path, min_quality, output_dir, fullFastaPath) {
+/**
+ * Define function to process all sequences from fastq
+ * @param {string} fastq_path
+ * @param {number} min_quality
+ * @param {string} fullFastaPath
+ * @returns {Promise<void>}
+ */
+async function fullSeqKit(fastq_path, min_quality, fullFastaPath) {
 	return new Promise((resolve, reject) => {
 		// Construct full shell command as a single string
 		const command = `seqkit seq --min-qual ${min_quality} ${fastq_path} | seqkit translate -f 6 -F > ${fullFastaPath}`;
@@ -27,7 +30,7 @@ async function fullSeqKit(fastq_path, min_quality, output_dir, fullFastaPath) {
 		seqkitFull.on("close", (code) => {
 			if (code === 0) {
 				console.log(`Translated sequences saved to: ${fullFastaPath}`);
-				resolve(fullFastaPath);
+				resolve();
 			} else {
 				console.error(`Process exited with code ${code}`);
 				reject(new Error(`Process failed with exit code ${code}`));
@@ -48,56 +51,21 @@ async function fullSeqKit(fastq_path, min_quality, output_dir, fullFastaPath) {
 	});
 }
 
-// Define function to cluster sequences for hmmer
-// @ts-ignore
-// async function getClusters(fastaPath, outputDir, tmpDir) {
-// 	return new Promise((resolve, reject) => {
-// 		const easyLinclust = spawn("mmseqs", [
-// 			"easy-linclust",
-// 			fastaPath,
-// 			path.join(outputDir, "test"),
-// 			tmpDir,
-// 			"--min-seq-id",
-// 			"0.9",
-// 		]);
-
-// 		easyLinclust.stdout.on("data", (data) => {
-// 			console.log(`stdout: ${data}`);
-// 		});
-
-// 		easyLinclust.stderr.on("data", (data) => {
-// 			console.error(`stderr: ${data}`);
-// 		});
-
-// 		easyLinclust.on("close", (code) => {
-// 			if (code === 0) {
-// 				// Define expected output files
-// 				const repSequences = path.join(outputDir, "test_rep_seq.fasta");
-// 				const clusterAssignments = path.join(outputDir, "test_cluster.tsv");
-// 				const allSequences = path.join(outputDir, "test_all_seqs.fasta");
-
-// 				resolve({
-// 					repSequences,
-// 					clusterAssignments,
-// 					allSequences,
-// 				});
-// 			} else {
-// 				reject(new Error(`Process exited with code ${code}`));
-// 			}
-// 		});
-// 	});
-// }
-
-// Define function to run hmmsearch
-// @ts-ignore
+/**
+ * Define function to run hmmsearch
+ * @param {string} modelPath
+ * @param {string} fastaPath
+ * @param {string} outputPath
+ * @returns {Promise<void>}
+ */
 async function runHMMSearch(modelPath, fastaPath, outputPath) {
 	// Return a Promise that wraps the asynchronous execution of hmmsearch
 	return new Promise((resolve, reject) => {
 		const hmmsearch = spawn("hmmsearch", [
 			"-E",
-			1e-5, // e-value threshold
+			"1e-5", // e-value threshold
 			"--domtblout",
-			outputPath,
+			outputPath, // hmmsearch output is written to specified output file
 			modelPath,
 			fastaPath,
 		]);
@@ -118,7 +86,7 @@ async function runHMMSearch(modelPath, fastaPath, outputPath) {
 				console.log(
 					`hmmsearch completed successfully. Output written to ${outputPath}`,
 				);
-				resolve("Success");
+				resolve();
 			} else {
 				// If exit code is non-zero, an error occurred
 				reject(new Error(`hmmsearch process exited with code ${code}`));
@@ -127,8 +95,16 @@ async function runHMMSearch(modelPath, fastaPath, outputPath) {
 	});
 }
 
-// Define function to parse full hmmsearch output
-// @ts-ignore
+/**
+ * Define function to parse full hmmsearch output
+ * @param {string} hmmerOut
+ * @returns {Promise<Array<{
+ * target_name: string,
+ * score: number,
+ * e_value: number,
+ * ali_from: number,
+ * ali_to: number}>>}
+ */
 async function parseFullHMMOutput(hmmerOut) {
 	try {
 		const lines = (await fs.promises.readFile(hmmerOut, "utf8")).split("\n");
@@ -157,128 +133,168 @@ async function parseFullHMMOutput(hmmerOut) {
 			parsedData.push(entry);
 		}
 
-		// Group by target_name and select the highest scoring row per target
+		// Group entries by target_name and select the highest scoring row per target
+		// Each entry represents a parsed hit (row) from hmmsearch output
+		/** @type {Record<string, {target_name: string, score: number, e_value: number, ali_from: number, ali_to: number} >} */
 		const bestEntries = {};
 
 		for (const entry of parsedData) {
 			const key = entry.target_name;
+			const entryForTarget = bestEntries[key];
 
 			// Keep the highest-scoring entry per target_name
-			// @ts-ignore
-			if (!bestEntries[key] || bestEntries[key].score < entry.score) {
-				// @ts-ignore
-				bestEntries[key] = entry;
+			if (entryForTarget === undefined) {
+				bestEntries[key] = entry; // if this is the first time encountering this target, save it to bestEntries
+			} else if (entryForTarget.score < entry.score) {
+				bestEntries[key] = entry; // if this entry of target is greater than the current bestEntries, replace
 			}
 		}
 
 		return Object.values(bestEntries);
 	} catch (error) {
-		// @ts-ignore
-		console.error(`Error parsing hmmsearch output: ${error.message}`);
+		if (error instanceof Error) {
+			console.error(`Error parsing hmmsearch output: ${error.message}`);
+		} else {
+			console.error("Unknown error occurred.");
+		}
 		throw error;
 	}
 }
 
-// Define function to parse FASTA file and store sequences by target_name
-// @ts-ignore
+/**
+ * Define function to parse FASTA file and store sequences by target_name
+ * @param {string} fastaFilePath
+ * @returns {Promise<Object<string, string>>}
+ */
 async function parseFasta(fastaFilePath) {
 	try {
 		const data = await fs.promises.readFile(fastaFilePath, "utf8");
 		const lines = data.trim().split("\n");
+		/** @type {Record<string, string>} */
 		const sequences = {};
 		let targetName = "";
 		let sequence = "";
 
 		for (const line of lines) {
 			if (line.startsWith(">")) {
-				// @ts-ignore
-				if (targetName) sequences[targetName] = sequence; // Save last sequence
+				if (targetName) {
+					sequences[targetName] = sequence; // Save last sequence
+				}
 				targetName = line.substring(1).split(" ")[0]; // Remove ">" and extra info
 				sequence = "";
 			} else {
 				sequence += line.trim();
 			}
 		}
-		// @ts-ignore
-		if (targetName) sequences[targetName] = sequence; // Save last entry
+
+		if (targetName) {
+			sequences[targetName] = sequence; // Save last entry
+		}
 
 		return sequences;
 	} catch (error) {
-		// @ts-ignore
-		console.error(`Error parsing FASTA file: ${error.message}`);
+		if (error instanceof Error) {
+			console.error(`Error parsing FASTA file: ${error.message}`);
+		} else {
+			console.error("Unknown error occurred.");
+		}
 		throw error;
 	}
 }
 
-// Define function to merge HMMER output and fasta sequences by target_name
-// @ts-ignore
+/**
+ * Define function to merge HMMER output and fasta sequences by target_name
+ * @param {string} hmmerFilePath
+ * @param {string} fastaFilePath
+ * @param {string} fastqFilePath
+ * @param {string} hmmFilePath
+ * @returns {Promise<Array<{
+ * target_name: string,
+ * score: number,
+ * e_value: number,
+ * ali_from: number,
+ * ali_to: number,
+ * sequence: string,
+ * FASTQ_filename: string,
+ * model_name: string,
+ * trimmed_seq: string,
+ * seq_len: number,
+ * start_pos: number}>>}
+ */
 async function mergeData(
 	hmmerFilePath,
 	fastaFilePath,
 	fastqFilePath,
 	hmmFilePath,
 ) {
+	// Parse HMMER output and FASTA file
 	const [hmmerData, fastaData] = await Promise.all([
 		parseFullHMMOutput(hmmerFilePath),
 		parseFasta(fastaFilePath),
 	]);
 
 	// Extract filenames from paths
-	const fastqFilename = fastqFilePath.split("/").pop();
-	const modelName = hmmFilePath.split("/").pop();
+	const fastqFilename = fastqFilePath.split("/").pop() || "";
+	const modelName = hmmFilePath.split("/").pop() || "";
 
-	return (
-		hmmerData
-			// @ts-ignore
-			.filter((entry) => fastaData[entry.target_name]) // Ensure sequence exists
-			.map((entry) => {
-				// @ts-ignore
-				const fullSequence = fastaData[entry.target_name];
-				const trimmedSeq = fullSequence.slice(entry.ali_from - 1, entry.ali_to); // Trim sequence
-				const seqLen = trimmedSeq.length;
+	// Create array
+	const mergedData = [];
 
-				// Extract start position from target_name
-				const match = entry.target_name.match(/frame=(-?\d+)/);
-				const startPos = match ? Number.parseInt(match[1], 10) : null;
+	for (const entry of hmmerData) {
+		const fullSequence = fastaData[entry.target_name]; // Extract full sequence for target_name from the fasta file
+		const trimmedSeq = fullSequence.slice(entry.ali_from - 1, entry.ali_to); // Trim sequence
+		const seqLen = trimmedSeq.length; // Calculate trimmed sequence length
 
-				// Remove "frame=" information from target_name
-				const cleanedTargetName = entry.target_name
-					.replace(/_frame=-?\d+/, "")
-					.trim();
+		// Extract start position from target_name
+		const match = entry.target_name.match(/frame=(-?\d+)/);
+		const startPos = match ? Number.parseInt(match[1], 10) : 0; // if no match is found, startPos is set to 0
 
-				return {
-					...entry,
-					target_name: cleanedTargetName,
-					sequence: fullSequence, // Attach full FASTA sequence
-					FASTQ_filename: fastqFilename,
-					model_name: modelName,
-					trimmed_seq: trimmedSeq,
-					seq_len: seqLen,
-					start_pos: startPos,
-				};
-			})
-	);
+		// Remove "frame=" information from target_name
+		const cleanedTargetName = entry.target_name
+			.replace(/_frame=-?\d+/, "")
+			.trim();
+
+		mergedData.push({
+			...entry,
+			target_name: cleanedTargetName,
+			sequence: fullSequence, // Attach full FASTA sequence
+			FASTQ_filename: fastqFilename,
+			model_name: modelName, // HMM filename
+			trimmed_seq: trimmedSeq,
+			seq_len: seqLen,
+			start_pos: startPos, // Start position for translation
+		});
+	}
+
+	return mergedData;
 }
 
-// Define function to count unique VH-VL pairs
-// @ts-ignore
-// Updated: process rows one at a time without reducing everything at once
+/**
+ * Define function to count unique VH-VL pairs
+ * @param {string} mergedDataPath
+ * @returns {Promise<{ vh_sequence: (string|null), vl_sequence: (string|null), count: number }[]>}
+ */
 async function countSeqs(mergedDataPath) {
 	return new Promise((resolve, reject) => {
-		const pairs = {}; // Object to store pairs
+		/**
+		 * @type {Object.<string, {target_name: string, FASTQ_filename: string, vh_sequence: (string|null), vl_sequence: (string|null)}>}
+		 * allSeqPairs: Object to store VH-VL sequence pairs, indexed by the unique key of target_name and FASTQ_filename
+		 */
+		const allSeqPairs = {}; // Object to store pairs
 
 		fs.createReadStream(mergedDataPath)
 			.pipe(csv())
-			.on("data", (row) => {
-				const isVH = row.model_name.toLowerCase().includes("vh");
-				const isVL = row.model_name.toLowerCase().includes("vl");
+			.on("data", (entry) => {
+				const isVH = entry.model_name.toLowerCase().includes("vh");
+				const isVL = entry.model_name.toLowerCase().includes("vl");
 
 				if (isVH || isVL) {
-					const { target_name, FASTQ_filename, trimmed_seq } = row;
-					const pairKey = `${target_name}_${FASTQ_filename}`;
+					const { target_name, FASTQ_filename, trimmed_seq } = entry; // Extract relevant columns from the entry
+					const targetFastqPair = `${target_name}_${FASTQ_filename}`; // define a unique combination of target_name and FASTQ_filename
 
-					if (!pairs[pairKey]) {
-						pairs[pairKey] = {
+					// Create a new entry for targetFastqPair if one does not exist already in allSeqPairs
+					if (!allSeqPairs[targetFastqPair]) {
+						allSeqPairs[targetFastqPair] = {
 							target_name,
 							FASTQ_filename,
 							vh_sequence: null,
@@ -286,30 +302,45 @@ async function countSeqs(mergedDataPath) {
 						};
 					}
 
-					if (isVH && pairs[pairKey].vh_sequence === null) {
-						pairs[pairKey].vh_sequence = trimmed_seq;
-					} else if (isVL && pairs[pairKey].vl_sequence === null) {
-						pairs[pairKey].vl_sequence = trimmed_seq;
+					// Check if current sequence is a VH sequence
+					if (isVH && allSeqPairs[targetFastqPair].vh_sequence === null) {
+						// If no VH sequence has been assigned yet, assign the trimmed sequence
+						allSeqPairs[targetFastqPair].vh_sequence = trimmed_seq;
+						// Check if current sequence is a VH sequence
+					} else if (
+						isVL &&
+						allSeqPairs[targetFastqPair].vl_sequence === null
+					) {
+						// If no VL sequence has been assigned yet, assign the trimmed sequence
+						allSeqPairs[targetFastqPair].vl_sequence = trimmed_seq;
 					}
 				}
 			})
 			.on("end", () => {
-				// Process pairs and count them
-				const completePairs = Object.values(pairs).filter(
+				// Process and count pairs
+				const completePairs = Object.values(allSeqPairs).filter(
 					(pair) => pair.vh_sequence && pair.vl_sequence,
 				);
 
+				/**
+				 * @type {Object.<string, {vh_sequence: (string|null), vl_sequence: (string|null), count: number}>}
+				 * pairCounts: Object to store the counts of unique VH-VL sequence pairs
+				 */
 				const pairCounts = {};
-				completePairs.forEach((pair) => {
+				for (const pair of completePairs) {
 					const { vh_sequence, vl_sequence } = pair;
-					const pairIdentifier = `${vh_sequence}_${vl_sequence}`;
+					const sequencePair = `${vh_sequence}_${vl_sequence}`; // define a unique identifier for VH/VL pair
 
-					if (!pairCounts[pairIdentifier]) {
-						pairCounts[pairIdentifier] = { vh_sequence, vl_sequence, count: 0 };
+					// Check if sequence pair already exists in pairCounts
+					if (!pairCounts[sequencePair]) {
+						// If not, initialize entry with VH + VL sequences and a count of 0
+						pairCounts[sequencePair] = { vh_sequence, vl_sequence, count: 0 };
 					}
-					pairCounts[pairIdentifier].count++;
-				});
+					// Increment count for this sequence pair
+					pairCounts[sequencePair].count++;
+				}
 
+				// Sort counts in descending order
 				const result = Object.values(pairCounts).sort(
 					(a, b) => b.count - a.count,
 				);
@@ -319,12 +350,15 @@ async function countSeqs(mergedDataPath) {
 	});
 }
 
-// Define function to write/append merged output to CSV
-// @ts-ignore
+/**
+ * Define function to write/append merged output to CSV
+ * @param {string} outputFile
+ * @param {Array<Record<string, any>>} data
+ * @param {boolean} append
+ */
 async function writeCsv(outputFile, data, append = false) {
 	try {
 		const header = Object.keys(data[0]);
-		// @ts-ignore
 		const rows = data.map((row) =>
 			header.map((col) => row[col] || "").join(","),
 		);
@@ -332,7 +366,7 @@ async function writeCsv(outputFile, data, append = false) {
 
 		// Check if the file exists and append data instead of overwriting
 		if (append && fs.existsSync(outputFile)) {
-			fs.appendFileSync(outputFile, "\n" + csvContent, "utf8");
+			fs.appendFileSync(outputFile, `\n${csvContent}`, "utf8");
 		} else {
 			fs.writeFileSync(
 				outputFile,
@@ -343,8 +377,11 @@ async function writeCsv(outputFile, data, append = false) {
 
 		console.log(`CSV updated: ${outputFile}`);
 	} catch (error) {
-		// @ts-ignore
-		console.error(`Error writing CSV: ${error.message}`);
+		if (error instanceof Error) {
+			console.error(`Error writing CSV: ${error.message}`);
+		} else {
+			console.error("Unknown error occurred.");
+		}
 		throw error;
 	}
 }
@@ -362,47 +399,23 @@ async function main() {
 	// Extract parameters from config object
 	const { output_path, min_quality, input_pairs } = config;
 
-	// Validate parameters from config
-	if (
-		!output_path ||
-		!min_quality ||
-		!Array.isArray(input_pairs) ||
-		input_pairs.length === 0
-	) {
-		console.error("Error: Missing required config values.");
-		return;
-	}
-
 	// Define paths for final output files
-	const date = new Date().toISOString().split("T")[0]; // Record date of job (YYYY-MM-DD)
-	const matchesOut = path.join(output_path, `ngs_hmm_matches_${date}.csv`); // need to add job ID to filename
-	const countsOut = path.join(output_path, `ngs_trimmed_counts_${date}.csv`);
+	const matchesOut = path.join(output_path, "ngs_hmm_matches.csv");
+	const countsOut = path.join(output_path, "ngs_trimmed_counts.csv");
 
 	// Loop over input pairs
 	for (const { fastq_path, model_path } of input_pairs) {
-		// Validate parameters
-		if (!fastq_path || !model_path) {
-			console.error("Error: Missing fastq_path or model_path in input pair.");
-			continue; // Skip this pair and move to the next
-		}
-
 		console.log(`Processing: ${fastq_path} with model ${model_path}`);
 
 		// Define intermediate output paths (these will be removed when processing is complete!)
-		const fullFastaPath = path.join(output_path, `full.fasta`);
-		const domtbloutPath = path.join(output_path, `hmmsearch_out.tbl`);
-		// const tempDir = path.join(output_path, "temp");
+		const fullFastaPath = path.join(output_path, "full.fasta");
+		const domtbloutPath = path.join(output_path, "hmmsearch_out.tbl");
 
 		try {
 			// Translate sequences in all 6 frames
-			await fullSeqKit(fastq_path, min_quality, output_path, fullFastaPath);
+			await fullSeqKit(fastq_path, min_quality, fullFastaPath);
 
-			// Cluster sequences
-			// const clustOut = await getClusters(fullFastaPath, output_path, tempDir);
-			// const repSequences = clustOut.repSequences;
-			//const clusterAssignments = clustOut.clusterAssignments
-
-			// Run HMMER on representative sequences from each cluster
+			// Run HMMER on all translated sequences
 			await runHMMSearch(model_path, fullFastaPath, domtbloutPath);
 
 			// Merge hmmsearch results with sequence information and trim based on match results
@@ -418,10 +431,13 @@ async function main() {
 			// Clean intermediate files
 			fs.unlinkSync(fullFastaPath);
 			fs.unlinkSync(domtbloutPath);
-			//fs.rmdirSync(tempDir);
 		} catch (error) {
-			// @ts-ignore
-			console.error(error.message);
+			if (error instanceof Error) {
+				console.error(error.message);
+			} else {
+				console.error("Unknown error occurred.");
+			}
+			throw error;
 		}
 	}
 
@@ -431,7 +447,12 @@ async function main() {
 		await writeCsv(countsOut, countData, true);
 		console.log("Final counts written to:", countsOut);
 	} catch (error) {
-		console.error("Error in countSeqs:", error.message);
+		if (error instanceof Error) {
+			console.error("Error in countSeqs:", error.message);
+		} else {
+			console.error("Unknown error occurred.");
+		}
+		throw error;
 	}
 }
 
